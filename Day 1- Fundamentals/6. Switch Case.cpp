7d954c77e395e560638cc40c76bcf8d1a6985804
Dhruv Kumar <dhruvkumar31712@gmail.com>
2026-01-08 10:22:22 +0530

    added switch case

---
+++ Day 1- Fundamentals/6. Switch Case.cpp
@@ -0,0 +1,37 @@
+//Switch Case
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int day;
+    cin >> day;
+
+    switch (day) {
+        case 1:
+            cout << "monday\n";
+            break;
+        case 2:
+            cout << "tuesday\n";
+            break;
+        case 3:
+            cout << "wednesday\n";
+            break;
+        case 4:
+            cout << "thursday\n";
+            break;
+        case 5:
+            cout << "friday\n";
+            break;
+        case 6:
+            cout << "saturday\n";
+            break;
+        case 7:
+            cout << "sunday\n";
+            break;
+        default:
+            cout << "invalid day\n";
+    }
+
+    return 0;
+}

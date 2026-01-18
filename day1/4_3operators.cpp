ed423032378d8a1de05fa02649d926c087c55f29
Dhruv Kumar <dhruvkumar31712@gmail.com>
2026-01-18 18:53:25 +0530

    Rename variable 'a' to 'x' in operators example

--- day1/4_3operators.cpp
+++ day1/4_3operators.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int a = 7;
+    int x = 7;
 
     cout << ((x<5) && (x<10))  << "\n";  // logical AND
     cout << ((x<5) || (x<4))   << "\n";  // logical OR
